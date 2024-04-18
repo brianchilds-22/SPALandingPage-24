8bb4394411927ff57da76b778e64685ab908d7b4
brianchilds-22 <46983745+brianchilds-22@users.noreply.github.com>
2024-04-18 10:13:21 -0600

    RodmapFootersections

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -17,6 +19,8 @@ const App = () => {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
