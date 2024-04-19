5f644f26ed2cc8124172ac515d25842923a8e684
brianchilds-22 <46983745+brianchilds-22@users.noreply.github.com>
2024-04-19 08:58:28 -0600

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/github.com/brianchilds-22/SPALandingPage-24",
+  base: "<REPO>",
   plugins: [react()],
 });
